af74b20964e43c92e9ea77268e9beff82e44a569
shadurin <igor.shadurin@gmail.com>
2019-12-21 15:19:22 +0300

    url generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [appId, setAppId] = useState(1);
+    const [url, setUrl] = useState('https://localhost:3000/bzz:/getlogin.eth/authorize');
+    const [redirectUrl, setRedirectUrl] = useState(window.location.href);
+
+    return (
+        <div className="container">
+            <h1>Authorize app example</h1>
+
+            <form method="get" action={url}>
+                <div className="form-group">
+                    <label htmlFor="url">URL</label>
+                    <input type="text" className="form-control" id="url"
+                           placeholder="App ID" value={url} onChange={e => setUrl(e.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="appId">Client ID</label>
+                    <input type="text" name="client_id" className="form-control" id="appId"
+                           placeholder="App ID" value={appId} onChange={e => setAppId(e.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="redirectUrl">Redirect URL</label>
+                    <input type="text" name="redirect_url" className="form-control" id="redirectUrl"
+                           placeholder="Redirect URL" value={redirectUrl}
+                           onChange={e => setRedirectUrl(e.target.value)}/>
+                </div>
+
+
+                <button type="submit" className="btn btn-primary">Authorize with GetLogin</button>
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
